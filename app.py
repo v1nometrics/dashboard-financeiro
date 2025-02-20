743ca77d2eaf8fd89856526b12c699f0a8336e42
v1nometrics <161180150+v1nometrics@users.noreply.github.com>
2025-02-20 17:18:18 -0300

    Update app (import os).py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import matplotlib.pyplot as plt
+import os
 
 
 st.set_page_config(layout="wide")
