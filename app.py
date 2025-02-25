bb70f02194e9a17259d26e4622f66418a0a327e7
v1nometrics <161180150+v1nometrics@users.noreply.github.com>
2025-02-25 11:00:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ authenticator.login()
 
 # Verificação do status da autenticação
 if st.session_state["authentication_status"]:
-    authenticator.logout("Logout", "sidebar")
+    authenticator.logout("Logout")
     st.write(f"Bem-vindo, {st.session_state['name']}!")
 elif st.session_state["authentication_status"] is False:
     st.error('Usuário/Senha inválido')
