95c20b0e30c55cb08ef9e4c612535f320889c80a
v1nometrics <161180150+v1nometrics@users.noreply.github.com>
2025-07-14 16:45:17 -0300

    credenciais_removidas_2.py

--- app.py
+++ app.py
@@ -503,8 +503,8 @@ if st.session_state["authentication_status"]:
     s3 = boto3.resource(
         service_name='s3',
         region_name='us-east-2',
-        aws_access_key_id='AKIA47GB733YQT2N7HNC',
-        aws_secret_access_key='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'
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
     )
 
     # ---------------------------------------------------
